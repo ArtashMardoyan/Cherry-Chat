82e85a3a39238d95c1aab5702256d3f82c7301c9
ArtashMardoyan <artashmardoyan@gmail.com>
2017-07-11 17:26:35 +0400

    added baseUrl for each handler

--- endpoints/handlers/home/index.js
+++ endpoints/handlers/home/index.js
@@ -1,9 +1,11 @@
+const config = require('../../../common/config/env');
 const meta = require('../../../common/meta');
 const _assign = require('lodash').assign;
 
 module.exports = (req, res) => {
     res.render('home', _assign({
-        title: 'Cherry Chat',
-        meta: meta.home
+        title: config.title,
+        meta: meta.home,
+        baseUrl: config.baseUrl
     }, req.locals));
 };
--- endpoints/handlers/signUp/index.js
+++ endpoints/handlers/signUp/index.js
@@ -1,5 +1,11 @@
+const config = require('../../../common/config/env');
+const meta = require('../../../common/meta');
+const _assign = require('lodash').assign;
+
 module.exports = (req, res) => {
-    res.render('signUp', {
-        title: 'Cherry Chat'
-    });
+    res.render('signUp', _assign({
+        title: config.title + ' | SignUp',
+        meta: meta.signUp,
+        baseUrl: config.baseUrl + '/signUp'
+    }, req.locals));
 };
