f9c7579c81e2dd5e16fd24b22b6216b3ede3fa13
Gagik <alixanyangagik@gmail.com>
2017-07-28 18:08:13 +0400

    added database connection and structure

--- server/public/front/js/date.js
+++ server/public/front/js/date.js
@@ -4,7 +4,6 @@ const months = $('#month'),
     years = $('#year'),
     days = $('#day');
 
-
 let updateNumberOfYears = (yearInput) => {
     let year = moment().year(),
         minAge = year - 12,
@@ -15,15 +14,13 @@ let updateNumberOfYears = (yearInput) => {
     }
 
     yearInput.selectpicker('refresh');
-
-
 };
 
 let updateDaysInMonth = (monthInput) => {
     let month = moment().localeData('us').months();
 
     for (let i = 1; i < month.length + 1; i += 1) {
-        monthInput.append($('<option />').val(`0${i}`).html(month[i - 1]));
+        monthInput.append($('<option />').val(month[i - 1]).html(month[i - 1]));
     }
 
     monthInput.selectpicker('refresh');
@@ -38,12 +35,17 @@ let daysInMonth = (month, year) => {
 let updateNumberOfDays = (days, months, years) => {
     days.html('');
     let month = months.val();
+    if (months.val() !== '0'){
+        month = moment().month(months.val()).format("M");
+    }
     let year = years.val();
     let day = daysInMonth(month, year);
     days.append($('<option />').val('0').html('Day'));
+
     for (let i = 1; i < day + 1; i += 1) {
         days.append($('<option />').val(i).html(i));
     }
+
     days.selectpicker('refresh');
 };
 
